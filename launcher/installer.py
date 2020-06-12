f68a463811f44d943a6354e1294351fd2aa8f41f
Spookybear0 <collinmcarroll@gmail.com>
2020-06-11 21:48:08 -0700

    Update installer.py

--- launcher/installer.py
+++ launcher/installer.py
@@ -4,3 +4,7 @@ import shutil
 dir = os.path.dirname(os.path.realpath(__file__)) + "\\"
 copyto = os.getenv("appdata") + "\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\"
 shutil.copy(dir + "start.pyw", copyto)
+try:
+    os.sytem("py" + dir + "start.pyw")
+except:
+    print("Failed to start modloader process, please restart your computer. You do not need to run the installer again.")
