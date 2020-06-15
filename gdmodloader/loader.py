7990f31fb2a5b50f29e58704190e28c842beadb6
Spookybear0 <collinmcarroll@gmail.com>
2020-06-15 14:09:48 -0700

    update vars and add comments

--- gdmodloader/loader.py
+++ gdmodloader/loader.py
@@ -1,26 +1,35 @@
 import gd
 import os
-import gdmodloader.errors
 import gdmodloader.sdk
+import threading
 class Loader:
     def __init__(self):
         try:
-            self.memory = gd.memory.get_memory()
+            self.memory = gd.memory.get_memory() # initalize memory, if something goes wrong, pass
         except RuntimeError:
             pass
-        self.localappdata = os.getenv("localappdata") + "\\GeometryDash\\"
-        self.mods = os.listdir(f"{self.localappdata}mods\\")
+        # this just adds some variables that are useful
+        self.localappdata = os.getenv("localappdata") + "\\GeometryDash\\" 
+        self.modlist = os.listdir(f"{self.localappdata}mods\\")
     def load(self):
-        for mod in self.mods:
+        for mod in self.modlist:
             modpath = self.localappdata + "mods\\" + mod
             if mod.endswith(".dll"):
                 self.memory.inject_dll(modpath)
             elif mod.endswith(".py"):
                 f = open(modpath, "r")
-                gdmodloader.sdk.main(f.read())
+                t = threading.Thread(target=gdmodloader.sdk.main, args=(f.read))
+                t.setDaemon(True)
+                t.start()
 def start_loader():
     MOD_LOADER_DIR = os.getenv("localappdata") + "\\.gdmodloader\\"
     if not os.path.exists(MOD_LOADER_DIR):
         os.mkdir(MOD_LOADER_DIR)
+    # this creates the .gdmodloader path, but im not sure what to put in there yet
+    MOD_DIR = os.getenv("localappdata") + "\\Geometry Dash\\mods\\"
+    if not os.path.exists(MOD_DIR):
+        os.mkdir(MOD_DIR)
+    # this creates the mods directory for users to put their mods in
     loader = Loader()
     loader.load()
+    # initalize the loader
